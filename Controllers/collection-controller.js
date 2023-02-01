8106b840e4d586cc668d80b991f6e45e488178da
mary3210 <maryrodri3210@gmail.com>
2023-02-01 09:37:54 -0600

    added get route in collections-controller

--- Controllers/collection-controller.js
+++ Controllers/collection-controller.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const router = express.Router()
-
 const { Post } = require('../models')
-
+const { Collection } = require('../models')
 const db = require('../models')
 
-
+router.get('/', async (req, res) => {
+    try {
+        console.log('a')
+        const allPosts = await Post.find({})
+        res.status(200).json(allPosts)
+    } catch (err) {
+        res.status(400).json({ error: err})
+    }
+})
 module.exports = router
