3e4976522bef9b3b65b7410fac93649243db09b7
mary3210 <maryrodri3210@gmail.com>
2023-02-01 13:41:35 -0600

    updated delete route in collection-controller

--- Controllers/collection-controller.js
+++ Controllers/collection-controller.js
@@ -13,4 +13,45 @@ router.get('/', async (req, res) => {
         res.status(400).json({ error: err})
     }
 })
+
+router.post('/', async (req, res) => {
+    console.log(req.body)
+    try{
+        const newCollection = await Collection.create(req.body)
+        res.status(201).json(newCollection)
+    } catch (err) {
+        res.status(400).json({ error: err.message })
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    try {
+        const foundCollection = await Collection.findById(req.params.id)
+        res.status(200).json(foundCollection)
+    } catch (err) {
+        res.status(400).json({ error: err })
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    try {
+
+        const updatedCollection = await Collection.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        res.status(200).json(updatedCollection)
+
+    } catch (err) {
+        res.status(400).json({ error: err })
+    }
+})
+router.delete('/:id', async (req, res) => {
+    try {
+        const deletedCollection = await Post.findByIdAndDelete(req.params.id)
+        res.status(200).json(deletedCollection)
+
+    } catch (err) {
+        res.status(400).json({ error: err })
+    }
+})
+
+
 module.exports = router
--- models/Collection.js
+++ models/Collection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 const CollectionSchema = new mongoose.Schema({
+    name: {type: String, required: true},
     posts: [{
         type: mongoose.Types.ObjectId,
         ref: 'Post',
