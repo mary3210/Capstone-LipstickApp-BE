27fb262cfa160895984f2c3aceb45c061de7bfd6
mary3210 <maryrodri3210@gmail.com>
2023-02-02 14:01:44 -0600

    added space

--- server.js
+++ server.js
@@ -24,4 +24,4 @@ app.get("/", (req, res) => {
 })
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`)); 
